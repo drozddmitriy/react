601c2d626269a7b68af407b437f9c4fbc9a83d76
drozd <drozd.dmitriy.etc@gmail.com>
2020-07-29 22:58:43 +0300

    Updates

---
+++ precache-manifest.ba476cb65ef77daa5c62e534d056deb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0900db56b14d452b6e55419be2a803a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "0906fbfce13abb17d31a",
+    "url": "/react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0906fbfce13abb17d31a",
+    "url": "/react/static/js/2.bf82450b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.bf82450b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2e281baec9cfc2141d",
+    "url": "/react/static/js/main.fbdc9907.chunk.js"
+  },
+  {
+    "revision": "55f260e18cc0f8fb068e",
+    "url": "/react/static/js/runtime-main.733639d2.js"
+  }
+]);
